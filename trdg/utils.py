5472b2ebd174495ac592cc1caadbc80e4afe424b
asyafEvinced <asya.frumkin@evinced.com>
2021-10-03 15:22:08 +0300

    Removed bounding boxes of spaces between words

--- trdg/utils.py
+++ trdg/utils.py
@@ -53,13 +53,6 @@ def mask_to_bboxes(mask, tess=False):
         try:
             color_tuple = ((i + 1) // (255 * 255), (i + 1) // 255, (i + 1) % 255)
             letter = np.where(np.all(mask_arr == color_tuple, axis=-1))
-            if space_thresh == 0 and letter:
-                x1 = min(bboxes[-1][2] + 1, np.min(letter[1]) - 1)
-                y1 = min(bboxes[-1][3] + 1, np.min(letter[0]) - 1) if not tess else min(mask_arr.shape[0] - np.min(letter[0]) + 2, bboxes[-1][1] - 1)
-                x2 = max(bboxes[-1][2] + 1, np.min(letter[1]) - 2)
-                y2 = max(bboxes[-1][3] + 1, np.min(letter[0]) - 2) if not tess else max(mask_arr.shape[0] - np.min(letter[0]) + 2, bboxes[-1][1] - 1)
-                bboxes.append((x1, y1, x2, y2))
-                space_thresh += 1
             bboxes.append((
                 max(0, np.min(letter[1]) - 1),
                 max(0, np.min(letter[0]) - 1) if not tess else max(0, mask_arr.shape[0] - np.max(letter[0]) - 1),
